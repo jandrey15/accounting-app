7e18a9d56359ede393410df32785fb721884c376
John Serrano <andrey_201015@hotmail.com>
2021-01-23 16:41:08 -0500

    Initial commit

--- pages/_app.js
+++ pages/_app.js
@@ -1,7 +1,11 @@
-import '../styles/globals.css'
+import '../styles/index.css'
 
 function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <div className="container mx-auto my-6">
+      <Component {...pageProps} />
+    </div>
+  )
 }
 
 export default MyApp
